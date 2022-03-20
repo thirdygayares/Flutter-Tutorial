c184cfadf2da5c6f5b30a7ee9ced7340d16e9cde
thirdy454 <65775625+thirdy454@users.noreply.github.com>
2022-03-20 10:25:05 +0800

    Create drawer.dart

---
+++ drawer.dart
@@ -0,0 +1,11 @@
+
+    drawer: new Drawer(
+      child: new ListView(
+        children: <Widget> [
+          new UserAccountsDrawerHeader(
+            accountName: Text('Thirdy Gayares'),
+            accountEmail: Text('gayaresthird@gmail.com')
+          )
+        ],
+      ),
+    ),
